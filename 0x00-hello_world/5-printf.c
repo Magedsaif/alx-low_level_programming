1a6d57251b6a3fee62295b14081b71a6e405d18d
Magedsaif <magedsaif21@gmail.com>
2023-03-16 23:41:33 +0200

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - prints a given phrase
+ *Return: always 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
