d100c6a3e9e01ab66daf8d8a3ecffdc0a3de7cce
Magedsaif <magedsaif21@gmail.com>
2023-03-18 00:48:21 +0200

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+*main-print alphabet
+*
+*Return: always return 0
+*/
+int main(void)
+{
+int a;
+char ch;
+for (a = 0; a < 10; a++)
+{
+putchar('0' + a);
+}
+for (ch = 'a'; ch >= 'f'; ch++)
+{
+putchar (ch);
+}
+putchar ('\n');
+return (0);
+}
