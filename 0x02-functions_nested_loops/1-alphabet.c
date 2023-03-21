2a0e8e545e134578fb45627060f935721e5bb7d2
Magedsaif <magedsaif21@gmail.com>
2023-03-21 14:19:50 +0200

    hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 #include<stdio.h>
 /**
 *
-* main - print alphabet 
-* Return: always return 0 uppon success 
+* main-print alphabet
+* Return: always return 0 uppon success
 **/
 void print_alphabet(void)
 {
 char ch;
-for (ch = 'a';ch <= 'z';ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 _putchar (ch);
 }
