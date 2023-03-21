f3bf0961c1f3a8ffa91fc236f6ec1f927de2c957
Magedsaif <magedsaif21@gmail.com>
2023-03-21 19:30:20 +0200

    task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,18 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar ('+');
+_putchar ('+');
 return (1);
 }
 if (n == '0')
 {
-putchar ('0');
+_putchar ('0');
 return (0);
 }
-if (n < 0)
+else
 {
-putchar ('-');
+_putchar ('-');
 return (-1);
 }
+_putchar('\n');
 }
