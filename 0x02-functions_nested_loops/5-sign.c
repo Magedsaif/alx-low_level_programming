996c72562213483690a699f3ded5c9aef3c70ecd
Magedsaif <magedsaif21@gmail.com>
2023-03-21 19:35:05 +0200

    task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,9 @@
 #include "main.h" 
 #include <stdio.h> 
 /**
-*
-*
+*print_sign - main function
+*@n: character to compare
+*Return: 1 if positive, 0 if zero, -1 if negative
 **/
 int print_sign(int n)
 {
@@ -13,7 +14,7 @@ return (1);
 }
 if (n < '0')
 {
-_putchar ('-1');
+_putchar ('-');
 return (-1);
 }
 else
