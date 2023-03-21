32579d49ae52297fb948795b79ea27f59a54672b
Magedsaif <magedsaif21@gmail.com>
2023-03-21 16:22:09 +0200

    task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include<stdio.h>
+/**
+*_islower - main function
+*
+*Return : 1 if true, 0 is false
+**/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+return (0);
+}
