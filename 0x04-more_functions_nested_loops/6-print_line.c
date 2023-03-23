4126ab0cfb12394e121e9f033f28bbda8fcc120a
Magedsaif <magedsaif21@gmail.com>
2023-03-24 01:24:10 +0200

    task

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,23 +2,15 @@
 
 /**
 *print_line- prints a straight line
-*@n:number of times the character _ should be printed 
-*Return: always 0
+*@n: number of times the character _ should be printed
+*Return: no return
 **/
 
 void print_line(int n)
 {
-int i;
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
-for (i = n; i <=n; i++)
+for (i = 0; i < n; i++)
 {
 _putchar('_');
 }
 _putchar('\n');
-}
 }
