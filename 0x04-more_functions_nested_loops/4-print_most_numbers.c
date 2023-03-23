a9a517efc3de73a25c7f142233aa1b049da976b8
Magedsaif <magedsaif21@gmail.com>
2023-03-23 23:57:16 +0200

    task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_most_numbers- prints the numbers exept 4 and 2
+*Return: no return
+**/
+
+void print_most_numbers(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+while (i != '2' || i != '4')
+{
+_putchar(i);
+}
+}
+_putchar('\n');
+}
