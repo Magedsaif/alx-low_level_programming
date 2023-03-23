3ad1c26a2537e2fec5d2efd20e060337946f7b00
Magedsaif <magedsaif21@gmail.com>
2023-03-24 00:42:04 +0200

    task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,16 @@ void more_numbers(void)
 {
 int i, j;
 
-for (i = 0; i <= 9; i++)
+for (i = 0; i < 10; i++)
 {
-for (j = 0; j <= 14; j++)
+for (j = 0; j < 15; j++)
 {
 if (j >= 10)
 {
 _putchar((j / 10) + 48);
 _putchar((j % 10) + 48);
 }
-_putchar('\n');
 }
+_putchar('\n');
 }
 }
