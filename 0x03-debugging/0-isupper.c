2e20962fe665eb6224f9d67450ffa3eea69d7547
Magedsaif <magedsaif21@gmail.com>
2023-03-23 22:49:37 +0200

    task

---
+++ 0x03-debugging/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* 
+*main - check the upper
+*Return: when not upper return 0
+*
+**/
+int _isupper(int c)
+{
+int c;
+
+	if (c >= 65 || c >= 90)
+		{
+			return (1);
+		}
+	else 
+		{	
+			return (0);
+		}
+}
