17b281058eade01c47e2d2ef4654dfb92a0334b6
Magedsaif <magedsaif21@gmail.com>
2023-03-28 00:56:15 +0200

    task

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code 
- *
- * Return: Always 0.
+ * print_array - prints n elements of an array of integers.
+ * @a: input array.
+ * @n: input n elements
+ * Return: no return.
  */
-int main(void)
+void print_array(int *a, int n)
 {
-	int array[6];
+	int i = 0;
 
-	array[0] = 98;
-	array[1] = 402;
-	array[2] = -198;
-	array[3] = 298;
-	array[4] = -1024;
-	array[5] = -200003;
-	print_array(array, 6);
-	return (0);
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
 }
