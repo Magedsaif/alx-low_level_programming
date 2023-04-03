039fcb8ac917daeffbb7b649b62a55996e985760
Magedsaif <magedsaif21@gmail.com>
2023-04-03 23:39:36 +0200

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,19 +12,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-int i, j;
-unsigned int count = 0;
+int a, b;
+int counter = 0;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-for (j = 0; accept[j] != '\0'; j++)
-{
-if (s[i] == accept[j])
-count++;
-break;
-}
-if (s[j] == '\0')
-return (count);
-}
-return (count);
+for (a = 0; s[a] != '\0'; a++)
+	{
+	if (s[a] != 32)
+		{	
+		for (b = 0; accept[b] != '\0'; b++)
+			{
+			if (s[a] == accept[b])
+				counter++;
+			}
+		}
+	else 
+		return (counter);	
+		
+	}
+return (counter);
 }
