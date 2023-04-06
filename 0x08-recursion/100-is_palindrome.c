20f53085158944abfc3df8a45096530cd1b1d4bb
Magedsaif <magedsaif21@gmail.com>
2023-04-06 23:42:44 +0200

    task 100 not complete yet

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * find_prime - checks for aprime number
+ * @n: a given number to check if its a prime number
+ * @i: a number to test if @n is a prime number
+ * Return: prime number @n
+ *
+*/
+
+
+
+/**
+* is_prime_number -  a function that returns 1
+* if the input integer is a prime number, otherwise return 0
+* @n: a given number to check if its a prime number
+* Return: 1 if the input integer is a prime number,
+* otherwise return 0.
+*/
+int is_palindrome(char *s)
+{
+
+}
