0688aad0635187800fa9ed389eee352a1a2beafe
Magedsaif <magedsaif21@gmail.com>
2023-04-06 02:47:43 +0200

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,25 +1,32 @@
 #include "main.h"
 /**
- * find_square - finds the square root of two numbers
- * @a: a number to get the square root of.
- * @b: a number to test if i could get the square root of @a
- * Return: square root of @a
+ * find_prime - checks for aprime number
+ * @n: a given number to check if its a prime number
+ * @b: a number to test if i @n is a prime number
+ * Return: return prime number @n
  *
 */
 
-int find_square(int a, int b)
+int find_prime(int n, int b)
 {
+	if (n <= 1 || n % b == 0) /*excludes non prime numbers*/
+		return (0);
+	else if (n == b) /*means it's a prime number*/
+		return (1);
+	else if (n > b) /*keeps adding to @b starting from 2 to get to prime num*/
+		find_prime(n, b + 1);
 
+	return (1);
 }
 
 /**
 * is_prime_number -  a function that returns 1
-if the input integer is a prime number, otherwise return 0
+* if the input integer is a prime number, otherwise return 0
 * @n: a given number to check if its a prime number
 * Return: 1 if the input integer is a prime number,
 * otherwise return 0.
 */
 int is_prime_number(int n)
 {
-
+	return (find_prime(n, 2));
 }
