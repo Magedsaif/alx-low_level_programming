9754f3cbc7998db979649d76961556533ef654e2
Magedsaif <magedsaif21@gmail.com>
2023-04-10 22:27:28 +0200

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,14 @@
 */
 int main(int argc, char *argv[])
 {
-if (argc == 3)
-{
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	return (0);
-}
-else
+if (argc != 3)
 {
 	printf("error\n");
 	return (1);
+}
+else
+{
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
 }
