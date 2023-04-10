5f10b52dee419b555bfcecbe95189942abcf0b2d
Magedsaif <magedsaif21@gmail.com>
2023-04-10 11:07:11 +0200

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - a function that takes arguments after the program name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
