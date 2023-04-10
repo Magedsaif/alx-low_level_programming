c30b6d301ddd786ef33ee2ca0ba2cdf98685cf04
Magedsaif <magedsaif21@gmail.com>
2023-04-10 22:33:04 +0200

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
 if (argc != 3)
 {
-	printf("error\n");
+	printf("Error\n");
 	return (1);
 }
 else
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - a function that takes arguments after the program name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if no number passed, 1 if there are symbols in input
+*/
+int main(int argc, char *argv[])
+{
+
+}
