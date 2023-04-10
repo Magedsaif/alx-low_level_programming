2a3d33daf985921dbc2f2cdce0dedb0b42c1aa8c
Magedsaif <magedsaif21@gmail.com>
2023-04-10 22:06:34 +0200

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - a function that takes arguments after the program name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on passing 2 arguments, 1 if less than two
+*/
+int main(int argc, char *argv[])
+{
+if (argc == 3)
+{
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
+else
+	{
+		printf("error");
+		printf("\n");
+		return (1);
+	}
+}
