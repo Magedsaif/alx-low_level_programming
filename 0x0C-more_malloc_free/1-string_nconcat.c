8a8584cc09925258773503bd009a382e097c7b70
Magedsaif <magedsaif21@gmail.com>
2023-04-14 01:39:27 +0200

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,6 @@
 #include"main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * *string_nconcat - a function that concatenates two strings.
  * @s1:first string to be concatenated .
