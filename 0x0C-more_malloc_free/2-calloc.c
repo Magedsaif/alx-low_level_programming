a48af1783449a8101130226d6137cd3bb54f2d3b
Magedsaif <magedsaif21@gmail.com>
2023-04-14 02:08:38 +0200

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,22 +7,28 @@
  * @size:no. of bytes for each array element
  *
  * Return:returns a pointer to the allocated memory.
- * NULL If nmemb or size is 0, then
- *NULL If malloc fails
+ * NULL If nmemb or size is 0
+ * NULL If malloc fails
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *str;
-unsigned int i;
+unsigned int a;
 
-str = malloc(size * sizeof(char));
-if (size == 0 || str == NULL || nmemb == 0)
+if (size == 0 || nmemb == 0)
 {
 	return (NULL);
 }
-for (i = 0; i < nmemb; i++)
+
+/*allocating memory by multiplying size by no. of elements*/
+str = malloc(size * nmemb);
+if (str == NULL)
 {
-	str[i] = 0;
+	return (NULL);
+}
+for (a = 0; a < nmemb * size; a++) /*The memory is set to zero*/
+{
+	str[a] = 0;
 }
 return (str);
 }
