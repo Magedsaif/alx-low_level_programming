9141b9fb1c1b31835ea81536a539ffde824c42e5
Magedsaif <magedsaif21@gmail.com>
2023-04-18 01:52:34 +0200

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - a structure made for information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
