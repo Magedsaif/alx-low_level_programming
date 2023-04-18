776f779fc4ec08dfef7b262a9f19182965fac133
Magedsaif <magedsaif21@gmail.com>
2023-04-18 03:48:50 +0200

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,41 +12,43 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *newdog;
-
+dog_t *newdog;/*declaring a pointer to a block of memory called newdog*/
 
 newdog = malloc(sizeof(dog_t)); /*allocating memory to anew dog to be stored*/
 
 if (newdog == NULL)
 	return (NULL);
-
-
-/*allocating memory to a new dog name to be stored*/
+if (name != NULL)/*allocating memory to a new dog name to be stored*/
+{
 newdog->name = malloc(strlen(name) + 1);
 
-if (name != NULL)
-{
-strcpy(newdog->name, name);/*copy the new dog name*/
-}
+if (newdog->name != NULL)
+	strcpy(newdog->name, name);/*copy the new dog name*/
 else
 {
 	free(newdog);
 	return (NULL);
 }
+}
+else
+	{return (NULL); }
 
 newdog->age = age; /*store the new age given*/
-
-/*allocating memory to a new dog owner to be stored*/
-newdog->owner = malloc(strlen(owner) + 1);
-
-if (owner != NULL)
+if (owner != NULL)/*allocating memory to a new dog owner to be stored*/
 {
-strcpy(newdog->owner, owner);/*copy the new dog owner*/
+newdog->owner = malloc(strlen(name) + 1);
+
+if (newdog->owner != NULL)
+	strcpy(newdog->owner, owner);/*copy the new dog name*/
+else
+{
+	free(newdog);
+	free(newdog->name);
+	return (NULL);
+}
 }
 else
 {
-	free(newdog->name);
-	free(name);
 	return (NULL);
 }
 return (newdog);
