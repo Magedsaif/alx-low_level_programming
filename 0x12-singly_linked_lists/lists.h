497ec5e14e1d0379aa1f9ff0c7963339f7e46075
Magedsaif <magedsaif21@gmail.com>
2023-04-28 03:02:52 +0200

    task 1

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+size_t print_list(const list_t *h);
+#endif
