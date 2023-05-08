ab2770770fae35f0d34c354349b7e02f13606d73
Magedsaif <magedsaif21@gmail.com>
2023-05-08 23:34:03 +0200

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,12 +16,7 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, written, len;
-
-	if (text_content = NULL)
-		return (-1);
-	len = strlen(text_content);
-	
+	int fd, written;
 
 	if (filename == NULL)
 		return (-1);
@@ -32,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		written = write(fd, text_content, len);
+		written = write(fd, text_content, strlen(text_content));
 		if (written == -1)
 			return (-1);
 	}
