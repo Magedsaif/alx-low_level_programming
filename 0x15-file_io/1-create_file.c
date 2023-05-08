3798b71d27da6f70a0d4474d21a09a205f99f79c
Magedsaif <magedsaif21@gmail.com>
2023-05-08 23:36:17 +0200

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,7 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, written, len;
-
-	len = strlen(text_content);
+	int fd, written;
 
 	if (filename == NULL)
 		return (-1);
@@ -29,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		written = write(fd, text_content, len);
+		written = write(fd, text_content, strlen(text_content));
 		if (written == -1)
 			return (-1);
 		if (written == 0)
