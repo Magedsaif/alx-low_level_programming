ed58f001b05d41def60f0a7723932de4157b552c
Magedsaif <magedsaif21@gmail.com>
2023-06-15 23:48:57 +0200

    task 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,61 @@
+#include"lists.h"
+/**
+ * insert_dnodeint_at_index -  a function that inserts a new node
+ * at a given position.
+ *
+ * @h: pointer to head of the list
+ * @idx:
+ * @n:
+ * Return: sum or 0 if list is empty
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp, *new_node;
+	unsigned int counter;
+
+	counter = 0;
+	temp = *h;
+	new_node = (dlistint_t *) malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	if ((*h) == NULL)
+	{
+		if (idx == 0)
+		{
+			*h = new_node;
+			return (new_node);
+		}
+		return (NULL);
+	}
+
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+
+	while (temp->next != NULL && counter != idx)
+	{
+		counter++;
+		temp = temp->next;
+	}
+
+	if (counter == idx)
+	{
+		new_node->prev = temp;
+		new_node->next = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = new_node;
+		temp->next = new_node;
+		return (new_node);
+	}
+	else
+		return (NULL);
+}
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,13 @@
+#include"lists.h"
+/**
+ * delete_dnodeint_at_index - a function that deletes the node
+ * at index index of a dlistint_t linked list.
+ *
+ * @head: pointer to head of the list
+ * @index: index
+ * Return: sum or 0 if list is empty
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+
+}
