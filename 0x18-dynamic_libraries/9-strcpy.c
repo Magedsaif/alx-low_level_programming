07087347426bdcf701dd627cfd85e33fd7b322cd
Magedsaif <magedsaif21@gmail.com>
2023-06-19 13:54:37 +0200

    task 0

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcpy - copies a string from src to dest
+ * @dest: char pointer
+ * @src: char pointer
+ * Return: char pointer copied
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest;
+	src;
+return (0);
+}
