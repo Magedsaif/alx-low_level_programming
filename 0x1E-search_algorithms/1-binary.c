885ef282974ce75f9c6ea331124b65c8951fe9aa
magedsaif <magedsaif21@gmail.com>
2023-10-10 09:58:15 +0200

    task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,6 +10,7 @@
  * You can assume that array will be sorted in ascending order
  * You can assume that value won’t appear more than once in array
  *
+ * Return: integar
  */
 int binary_search(int *array, size_t size, int value)
 {
